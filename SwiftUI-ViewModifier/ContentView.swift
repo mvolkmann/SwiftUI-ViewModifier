52f4b4e853ada1947b903304a11267631810f25e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-28 13:45:47 -0500

    initial version

--- SwiftUI-ViewModifier/ContentView.swift
+++ SwiftUI-ViewModifier/ContentView.swift
@@ -1,16 +1,67 @@
-//
-//  ContentView.swift
-//  SwiftUI-ViewModifier
-//
-//  Created by R. Mark Volkmann on 10/28/21.
-//
-
 import SwiftUI
+
+struct Collapsable: ViewModifier {
+    private static let diameter = CGFloat(120)
+    private static var radius: CGFloat { diameter / 2 }
+    
+    var color: Color
+    
+    @State var showContent = true
+    
+    var halfCircle: some View {
+        Circle()
+            .trim(from: 0, to: 0.5)
+            .fill(.yellow)
+            .frame(width: Collapsable.diameter, height: Collapsable.radius)
+            .offset(x: 0, y: -16)
+    }
+    
+private func toggle() {
+    withAnimation {
+        showContent.toggle()
+        }
+    }
+    
+    func body(content: Content) -> some View {
+        VStack {
+            if showContent {
+                HStack {
+                    Spacer()
+                    content
+                    Spacer()
+                }.padding().border(color, width: 5)
+            }
+            HStack {
+                Spacer()
+                ZStack {
+                    Image(systemName: "chevron.down")
+                        .resizable()
+                        .frame(width: Collapsable.radius / 3, height: Collapsable.radius / 4)
+                        .onTapGesture { toggle() }
+                        .rotationEffect(Angle.degrees(showContent ? 180 : 0))
+                        .offset(x: 0, y: -2)
+                        .background(halfCircle)
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+extension View {
+    func collapsable(color: Color = .black) -> some View {
+        modifier(Collapsable(color: color))
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+            Text("This is the second line.")
+            //}.modifier(CollapsableModifier(color: .blue)) // long way
+        }
+        .collapsable(color: .green) // short way
     }
 }
 
