968c480859ecb4d32fc7b2d45a894d3556bb7719
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-28 10:47:50 -0500

    Initial Commit

---
+++ SwiftUI-ViewModifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-ViewModifier
+//
+//  Created by R. Mark Volkmann on 10/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-ViewModifier/SwiftUI_ViewModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ViewModifierApp.swift
+//  SwiftUI-ViewModifier
+//
+//  Created by R. Mark Volkmann on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ViewModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
